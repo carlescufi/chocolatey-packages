d0cc8202874d874de696412062c5083edd66e535
Carles Cufi <carles.cufi@nordicsemi.no>
2018-09-28 19:13:56 +0200

    Fix rsvg-convert install script
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- packages/rsvg-convert/tools/chocolateyinstall.ps1
+++ packages/rsvg-convert/tools/chocolateyinstall.ps1
@@ -3,7 +3,8 @@
 $packageName= 'rsvg-convert'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$zFile = Write-Host "Getting rsvg-convert .7z"; Get-Item "$toolsDir\rsvg-convert-*.7z"
+Write-Host "Getting rsvg-convert .7z"
+$zFile = Get-Item "$toolsDir\rsvg-convert-*.7z"
 
 Get-ChocolateyUnzip -FileFullPath $zfile -Destination $toolsDir
 
