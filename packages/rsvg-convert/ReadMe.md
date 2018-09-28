296a493a4577219ab475b8d19e84b03b47d883eb
Carles Cufi <carles.cufi@nordicsemi.no>
2018-09-28 19:03:18 +0200

    New rsvg-convert package
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

---
+++ packages/rsvg-convert/ReadMe.md
@@ -0,0 +1,3 @@
+﻿## rsvg-convert
+
+librsvg is a free software SVG rendering library written as part of the GNOME project, intended to be lightweight and portable. rsvg-convert is a tool to convert SVG files from said library. 
---
+++ packages/rsvg-convert/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName= 'rsvg-convert'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$zFile = Write-Host "Getting rsvg-convert .7z"; Get-Item "$toolsDir\rsvg-convert-*.7z"
+
+Get-ChocolateyUnzip -FileFullPath $zfile -Destination $toolsDir
+
