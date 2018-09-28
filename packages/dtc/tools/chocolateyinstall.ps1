6ffdf5bf700563d61c1097cf7fb1e7d7dec42cd0
Carles Cufi <carles.cufi@nordicsemi.no>
2018-09-28 18:57:11 +0200

    Add missing gcc-libs for 32-bit dtc
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- packages/dtc/tools/chocolateyinstall.ps1
+++ packages/dtc/tools/chocolateyinstall.ps1
@@ -3,6 +3,10 @@
 $packageName= 'dtc-msys2'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$libxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Getting x64 bit libs .pkg.tar.xz"; Get-Item "$toolsDir\gcc-libs-*x86_64.pkg.tar.xz"
+} else { Write-Host "Getting x32 bit libs .pkg.tar.xz"; Get-Item "$toolsDir\gcc-libs-*i686.pkg.tar.xz" }
+
 $rtxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
          Write-Host "Getting x64 bit core .pkg.tar.xz"; Get-Item "$toolsDir\msys2-runtime-*x86_64.pkg.tar.xz"
 } else { Write-Host "Getting x32 bit core .pkg.tar.xz"; Get-Item "$toolsDir\msys2-runtime-*i686.pkg.tar.xz" }
@@ -11,8 +15,13 @@ $dtcxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true')
          Write-Host "Getting x64 bit dtc .pkg.tar.xz"; Get-Item "$toolsDir\dtc-*x86_64.pkg.tar.xz"
 } else { Write-Host "Getting x32 bit dtc .pkg.tar.xz"; Get-Item "$toolsDir\dtc-*i686.pkg.tar.xz" }
 
+Get-ChocolateyUnzip -FileFullPath $libxvfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $rtxvfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $dtcxvfile -Destination $toolsDir
+
+$libtarFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Getting x64 bit libs .pkg.tar"; Get-Item "$toolsDir\gcc-libs-*x86_64.pkg.tar"
+} else { Write-Host "Getting x32 bit libs .pkg.tar"; Get-Item "$toolsDir\gcc-libs-*i686.pkg.tar" }
 
 $rttarFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
          Write-Host "Getting x64 bit core .pkg.tar"; Get-Item "$toolsDir\msys2-runtime-*x86_64.pkg.tar"
@@ -25,6 +34,7 @@ $dtctarFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true'
 Get-ChocolateyUnzip -FileFullPath $dtctarfile -Destination $toolsDir
 # Get file list for DTC
 $dtcFiles = Get-ChildItem $toolsDir\usr\bin | ForEach-Object { $_.FullName }
+Get-ChocolateyUnzip -FileFullPath $libtarfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $rttarfile -Destination $toolsDir
 
 # don't need tars anymore
@@ -32,7 +42,7 @@ Remove-Item ($toolsDir + '\*.' + 'xz')
 Remove-Item ($toolsDir + '\*.' + 'tar')
 
 # Clean up \usr\bin folder, we only need a set of files
-$rtFiles = "$toolsDir\usr\bin\msys-2.0.dll","$toolsDir\usr\bin\cygpath.exe"
+$rtFiles = "$toolsDir\usr\bin\msys-2.0.dll","$toolsDir\usr\bin\cygpath.exe", "$toolsDir\usr\bin\msys-gcc_s-1.dll"
 
 $keepFiles = $dtcFiles + $rtFiles
 $allFiles = Get-ChildItem $toolsDir\usr\bin | ForEach-Object { $_.FullName }
