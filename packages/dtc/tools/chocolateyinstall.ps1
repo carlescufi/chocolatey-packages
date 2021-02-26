994ac88a637b763662dc61192d1b5878bb1669df
Carles Cufi <carles.cufi@nordicsemi.no>
2021-02-26 14:18:06 +0100

    dtc: Update to DTC 1.5.1
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- packages/dtc/tools/chocolateyinstall.ps1
+++ packages/dtc/tools/chocolateyinstall.ps1
@@ -3,6 +3,10 @@
 $packageName= 'dtc-msys2'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
+$yamlxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Getting x64 bit yaml .pkg.tar.xz"; Get-Item "$toolsDir\libyaml-*x86_64.pkg.tar.xz"
+} else { Write-Host "Getting x32 bit yaml .pkg.tar.xz"; Get-Item "$toolsDir\libyaml-*i686.pkg.tar.xz" }
+
 $libxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
          Write-Host "Getting x64 bit libs .pkg.tar.xz"; Get-Item "$toolsDir\gcc-libs-*x86_64.pkg.tar.xz"
 } else { Write-Host "Getting x32 bit libs .pkg.tar.xz"; Get-Item "$toolsDir\gcc-libs-*i686.pkg.tar.xz" }
@@ -15,9 +19,14 @@ $dtcxvFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true')
          Write-Host "Getting x64 bit dtc .pkg.tar.xz"; Get-Item "$toolsDir\dtc-*x86_64.pkg.tar.xz"
 } else { Write-Host "Getting x32 bit dtc .pkg.tar.xz"; Get-Item "$toolsDir\dtc-*i686.pkg.tar.xz" }
 
+Get-ChocolateyUnzip -FileFullPath $yamlxvfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $libxvfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $rtxvfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $dtcxvfile -Destination $toolsDir
+
+$yamltarFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
+         Write-Host "Getting x64 bit libs .pkg.tar"; Get-Item "$toolsDir\libyaml-*x86_64.pkg.tar"
+} else { Write-Host "Getting x32 bit libs .pkg.tar"; Get-Item "$toolsDir\libyaml-*i686.pkg.tar" }
 
 $libtarFile = if ((Get-ProcessorBits 64) -and $env:chocolateyForceX86 -ne 'true') {
          Write-Host "Getting x64 bit libs .pkg.tar"; Get-Item "$toolsDir\gcc-libs-*x86_64.pkg.tar"
@@ -36,13 +45,14 @@ Get-ChocolateyUnzip -FileFullPath $dtctarfile -Destination $toolsDir
 $dtcFiles = Get-ChildItem $toolsDir\usr\bin | ForEach-Object { $_.FullName }
 Get-ChocolateyUnzip -FileFullPath $libtarfile -Destination $toolsDir
 Get-ChocolateyUnzip -FileFullPath $rttarfile -Destination $toolsDir
+Get-ChocolateyUnzip -FileFullPath $yamltarfile -Destination $toolsDir
 
 # don't need tars anymore
 Remove-Item ($toolsDir + '\*.' + 'xz')
 Remove-Item ($toolsDir + '\*.' + 'tar')
 
 # Clean up \usr\bin folder, we only need a set of files
-$rtFiles = "$toolsDir\usr\bin\msys-2.0.dll","$toolsDir\usr\bin\cygpath.exe", "$toolsDir\usr\bin\msys-gcc_s-1.dll"
+$rtFiles = "$toolsDir\usr\bin\msys-2.0.dll","$toolsDir\usr\bin\cygpath.exe", "$toolsDir\usr\bin\msys-gcc_s-1.dll", "$toolsDir\usr\bin\msys-yaml-0-2.dll"
 
 $keepFiles = $dtcFiles + $rtFiles
 $allFiles = Get-ChildItem $toolsDir\usr\bin | ForEach-Object { $_.FullName }
